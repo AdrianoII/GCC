29cbf5a9643aa278ed4604cd1830462f8256d064
AdrianoII <adrianocorbelinoii@gmail.com>
2021-07-25 23:39:54 -0400

    Add source file abstraction

---
+++ src/file_handler/file_handler.c
@@ -0,0 +1,91 @@
+//
+// Created by adrianoii on 24/07/2021.
+//
+#include "file_handler.h"
+#include <stdlib.h>
+#include "../exceptions/exceptions_handler.h"
+
+file_t *file_init(const char *file_path)
+{
+    file_t *new_file = calloc(1, sizeof(file_t));
+
+    if (new_file == NULL)
+    {
+        throw_exception(ALLOCATION_FAILED);
+    }
+
+    new_file->path = file_path;
+    new_file->line = 0;
+    new_file->col = 0;
+    new_file->is_fresh_line = false;
+
+    new_file->p_file = fopen(new_file->path, "r");
+    if (new_file->p_file == NULL)
+    {
+        throw_exception(INVALID_FILE);
+    }
+
+    return new_file;
+}
+
+int file_get_next_char(file_t *file)
+{
+    file->actual_char = fgetc(file->p_file);
+
+    if (file->actual_char == '\n')
+    {
+        ++file->line;
+        file->col = 0;
+//        file->is_fresh_line = true;
+    }
+    else if (file->is_fresh_line)
+    {
+        file->is_fresh_line = false;
+    } else
+    {
+        ++file->col;
+    }
+
+    return file->actual_char;
+}
+
+void file_rollback_byte(file_t *file)
+{
+    if (file->line == 0 && file->col == 0)
+    {
+        throw_exception(INVALID_FILE_ROLLBACK_CHAR);
+    }
+    fseek(file->p_file, -1, SEEK_CUR); // "abc\n"
+    if (file->actual_char == '\n')
+    {
+        long int old_col = 0;
+        fseek(file->p_file, -1, SEEK_CUR);
+
+        while ((file->actual_char = fgetc(file->p_file)) != '\n')
+        {
+            ++old_col;
+            // try to rollback the cursor 2 bytes
+            for (size_t i = 0; (ftell(file->p_file) != 0) && i < 2; i++)
+            {
+                fseek(file->p_file, -1, SEEK_CUR);
+            }
+
+            if (ftell(file->p_file) == 0)
+            {
+                ++old_col;
+                break;
+            }
+        }
+
+        while ((file->actual_char = fgetc(file->p_file)) != '\n');
+
+        fseek(file->p_file, -1, SEEK_CUR);
+
+        --file->line;
+        file->col = old_col;
+    } else
+    {
+        --file->col;
+    }
+
+}
---
+++ src/file_handler/file_handler.h
@@ -0,0 +1,27 @@
+//
+// Created by adrianoii on 24/07/2021.
+//
+
+#ifndef GCC_FILE_HANDLER_H
+#define GCC_FILE_HANDLER_H
+
+#include <stdio.h>
+#include "../string/string.h"
+
+typedef struct
+{
+    FILE *p_file;
+    int actual_char;
+    const char *path;
+    size_t line;
+    size_t col;
+    bool is_fresh_line;
+} file_t;
+
+file_t *file_init(const char *file_path);
+
+int file_get_next_char(file_t *file);
+
+void file_rollback_byte(file_t *file);
+
+#endif //GCC_FILE_HANDLER_H
