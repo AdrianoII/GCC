4e372407b052d26e647b071baaa094312489c8de
AdrianoII <adrianocorbelinoii@gmail.com>
2021-07-25 23:40:49 -0400

    Add source token abstraction

---
+++ src/lexical/lexical_token.c
@@ -0,0 +1,84 @@
+//
+// Created by adrianoii on 23/07/2021.
+//
+
+#include "lexical_token.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../exceptions/exceptions_handler.h"
+
+const char *lexical_token_class_to_string(lexical_token_class_t class)
+{
+    //TODO: Maybe set the vector as a global
+    const char *token_class_mapper[] = {
+            "INVALID_TOKEN_CLASS",
+            "BRACKET_COMMENT",
+            "SLASH_COMMENT",
+            "IDENTIFIER",
+            "INTEGER",
+            "REAL",
+            "KEYWORD",
+            "SYMBOL",
+            "SEPARATORS",
+    };
+
+    return token_class_mapper[class];
+}
+
+token_t *token_init()
+{
+    token_t *new_token = calloc(1, sizeof(token_t));
+
+    if (new_token == NULL)
+    {
+        throw_exception(ALLOCATION_FAILED);
+    }
+
+    new_token->value = string_init();
+
+    token_reset(new_token);
+
+    return new_token;
+}
+
+void token_reset(token_t *token)
+{
+    token->start_position = 0;
+    token->end_position = 0;
+    token->line = 0;
+    token->token_class = INVALID_TOKEN_CLASS;
+    string_reset(token->value);
+}
+
+void token_destroy(token_t *token)
+{
+    if (token != NULL)
+    {
+        string_destroy(token->value);
+    }
+    free(token);
+}
+
+void token_log(token_t *token)
+{
+    printf("TOKEN:\n");
+    if (token == NULL)
+    {
+        printf("{}\n");
+        return;
+    }
+
+    printf("{\n");
+    printf("\ttoken_class: \"%s\"\n", lexical_token_class_to_string(token->token_class));
+    printf("\tline: %zu\n", token->line);
+    printf("\tstart_position: %zu\n", token->start_position);
+    printf("\tend_position: %zu\n", token->end_position);
+    printf("\tvalue: ");
+    string_log(token->value);
+    printf("}\n");
+}
+
+void token_pretty_log(token_t *token)
+{
+    printf("< %s : %s >\n", lexical_token_class_to_string(token->token_class), token->value->buffer);
+}
---
+++ src/lexical/lexical_token.h
@@ -0,0 +1,43 @@
+//
+// Created by adrianoii on 23/07/2021.
+//
+
+#ifndef GCC_LEXICAL_TOKEN_H
+#define GCC_LEXICAL_TOKEN_H
+
+#include <stddef.h>
+#include "../string/string.h"
+
+typedef enum
+{
+    INVALID_TOKEN_CLASS,
+    BRACKET_COMMENT, // {}
+    SLASH_COMMENT, // /**/
+    IDENTIFIER, // i | abc |a2bc3
+    INTEGER, // 1 | 123 | 11564869
+    REAL, // 1.1 | 1.43333
+    KEYWORD, // if | then | while | do | write | read | else | begin | end | program | real | integer | procedure | var
+    SYMBOL, // ( | ) | * | / | + | - | <> | >= | <= | > | < | $ | : | , | ; | = | == | := | .
+    SEPARATORS, // ' ' | \t | \n
+} lexical_token_class_t;
+
+const char *lexical_token_class_to_string(lexical_token_class_t class);
+
+typedef struct Token
+{
+    lexical_token_class_t token_class;
+    size_t line;
+    size_t start_position;
+    size_t end_position;
+    string_t *value; // Optional
+} token_t;
+
+token_t *token_init();
+
+void token_reset(token_t *token);
+
+void token_log(token_t *token);
+
+void token_pretty_log(token_t *token);
+
+#endif //GCC_LEXICAL_TOKEN_H
