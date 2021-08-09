d482605d1da183d73573ccaf0fed83ba087bd63e
AdrianoII <adrianocorbelinoii@gmail.com>
2021-08-08 23:37:37 -0400

    Update lexical_tests.c to use the -sa argument

--- tests/lexical_tests.c
+++ tests/lexical_tests.c
@@ -19,15 +19,14 @@ void assert_valid_output_file(const char *file_name);
 void assert_valid_output_file(const char *file_name)
 {
     char cmd_buffer[1024] = {'\0'};
-    int diff_return_code = 0;
-    snprintf(cmd_buffer, 1023, "./" GCC_PATH " " TESTS_PATH "%s.txt > " OUTPUTS_PATH "%s_out.txt",
+    snprintf(cmd_buffer, 1023, "./" GCC_PATH " " TESTS_PATH "%s.txt -sa l > " OUTPUTS_PATH "%s_out.txt",
              file_name, file_name);
     printf("EXECUTE: %s\n", cmd_buffer);
     system(cmd_buffer);
     snprintf(cmd_buffer, 1023, "diff " OUTPUTS_PATH "%s_out.txt " TEMPLATES_PATH "%s_template.txt",
         file_name, file_name);
     printf("DIFF: %s\n", cmd_buffer);
-    diff_return_code = system(cmd_buffer);
+    int diff_return_code = system(cmd_buffer);
     if (diff_return_code != 0)
     {
         snprintf(cmd_buffer, 1023, "TEST FAILED: %s.", file_name);
